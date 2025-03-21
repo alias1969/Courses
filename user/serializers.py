a4b88970c5c519b529a3c5f44ffe1e6ca51955a6
Irina Kamneva <brynza80@gmail.com>
2025-03-21 12:49:57 +0300

    home task for lesson 32.2

--- user/serializers.py
+++ user/serializers.py
@@ -5,12 +5,17 @@ from .models import Payment, User
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    """Serializer для платежей"""
     class Meta:
         model = Payment
         fields = "__all__"
 
 
 class UserSerializer(ModelSerializer):
+    """Serializer для пользователя"""
+    # Отображаем платежи пользователя
+    payments = PaymentSerializer(source="payments_set", many=True, read_only=True)
+
     class Meta:
         model = User
         fields = "__all__"
